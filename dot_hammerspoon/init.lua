6ccf591e13fc27b73931348b90bf23db770cae18
Luis Cruz <luis.cruz@hqo.co>
2022-06-14 12:52:42 -0400

    Hammerspoon configuration file

---
+++ dot_hammerspoon/init.lua
@@ -0,0 +1,36 @@
+require("hs.ipc")
+
+-- ================================ Setup Logging =================================
+
+logger = hs.logger.new('windowManager')
+logger.setLogLevel('info')
+
+-- ========================== Spoons ===========================
+
+arrangeDesktop = hs.loadSpoon('ArrangeDesktop')
+arrangeDesktop.logger.setLogLevel('info')
+
+-- ========================== Desktop Layout Management ===========================
+
+hs.application.enableSpotlightForNameSearches(true)
+hs.window.filter.ignoreAlways['Firefox Developer EditionCP Web Content'] = true
+hs.window.filter.ignoreAlways['Firefox Developer EditionCP WebExtensions'] = true
+hs.window.filter.ignoreAlways['Firefox Developer EditionCP RDD Process'] = true
+hs.window.filter.ignoreAlways['Firefox Developer EditionCP Privileged Content'] = true
+hs.window.filter.ignoreAlways['Firefox Developer EditionCP Socket Process'] = true
+hs.window.filter.ignoreAlways['Slack Helper (Renderer)'] = true
+hs.window.filter.ignoreAlways['TweetDeck Networking'] = true
+
+-- ================================== Setup Menu ==================================
+
+menubar = hs.menubar.new()
+menubar:setIcon(hs.image.imageFromName("NSHandCursor"))
+
+if menubar then
+    local menuItems = {}
+
+    menuItems = arrangeDesktop:addMenuItems(menuItems)
+
+    menubar:setMenu(menuItems)
+end
+
