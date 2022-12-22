86d76076f64ad6765c791633ec6fd9a8d4c6169e
sprak <sprak3000+github@gmail.com>
2022-12-21 22:05:21 -0500

    autojump change

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 # Setup autojump
-[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+[ -f $(brew --prefix)/share/autojump/autojump.fish ]; and source $(brew --prefix)/share/autojump/autojump.fish
 
 # Load asdf
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
