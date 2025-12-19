24ef7b436ffd15b0deec754a34142449f57d0a68
Luis Cruz <sprak3000+github@gmail.com>
2025-12-18 20:11:47 -0500

    Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,5 @@
 # Setup autojump
 [ -f $(brew --prefix)/share/autojump/autojump.fish ]; and source $(brew --prefix)/share/autojump/autojump.fish
 
-# Load asdf
-source /opt/homebrew/opt/asdf/libexec/asdf.fish
-
+# Setup vfox
+vfox activate fish | source
